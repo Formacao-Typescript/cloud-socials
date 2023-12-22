2f147f210eb56fee7482c1e25a2f7b05964dab57
Lucas Santos <lhs.santoss@gmail.com>
2023-12-23 00:46:18 +0100

    chore: remove semi
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>

--- src/data/db.ts
+++ src/data/db.ts
@@ -1,7 +1,7 @@
-import { path } from '../deps.ts';
+import { path } from '../deps.ts'
 
 export function openKv() {
-	const dbPath = path.resolve(path.dirname(path.fromFileUrl(import.meta.url)), '.db/kv.sqlite');
-	Deno.mkdirSync(path.dirname(dbPath), { recursive: true });
-	return Deno.openKv(dbPath);
+	const dbPath = path.resolve(path.dirname(path.fromFileUrl(import.meta.url)), '.db/kv.sqlite')
+	Deno.mkdirSync(path.dirname(dbPath), { recursive: true })
+	return Deno.openKv(dbPath)
 }
--- src/networks/install.ts
+++ src/networks/install.ts
@@ -1,5 +1,5 @@
-import { AppConfig } from '../config.ts';
-import { oak } from '../deps.ts';
+import { AppConfig } from '../config.ts'
+import { oak } from '../deps.ts'
 
 export enum NetworkList {
 	Twitter = 'twitter',
@@ -8,17 +8,17 @@ export enum NetworkList {
 }
 
 export const install = async (app: oak.Application, { loggers }: AppConfig) => {
-	loggers.default.debug('Loading networks');
+	loggers.default.debug('Loading networks')
 	for (const network of Object.values(NetworkList)) {
 		try {
-			loggers.default.info(`Installing network ${network}`);
-			const networkRouter: oak.Router = (await import(`./${network}.ts`))['default'];
-			app.use(networkRouter.routes());
-			app.use(networkRouter.allowedMethods());
-			loggers.default.info(`Installed network ${network}`);
+			loggers.default.info(`Installing network ${network}`)
+			const networkRouter: oak.Router = (await import(`./${network}.ts`))['default']
+			app.use(networkRouter.routes())
+			app.use(networkRouter.allowedMethods())
+			loggers.default.info(`Installed network ${network}`)
 		} catch (err) {
-			loggers.error.warning(`Failed to install network ${network}`);
-			loggers.error.error((err as Error).message);
+			loggers.error.warning(`Failed to install network ${network}`)
+			loggers.error.error((err as Error).message)
 		}
 	}
-};
+}
