2ed2fedaec60e436366bc6a98b694e9b5ecd8b96
Lucas Santos <lhs.santoss@gmail.com>
2023-12-26 23:37:50 +0100

    fix: fmt
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>

--- src/domain/LinkedinPost.ts
+++ src/domain/LinkedinPost.ts
@@ -1,80 +1,80 @@
 import {
-  LinkedinMedia,
-  LinkedinMediaArticle,
-  LinkedinMediaAsset,
-  LinkedinMediaTypes
+	LinkedinMedia,
+	LinkedinMediaArticle,
+	LinkedinMediaAsset,
+	LinkedinMediaTypes,
 } from '../clients/LinkedinClient.ts'
 
 type LinkedinMediaGeneral = {
-  type: Exclude<LinkedinMedia['type'], 'article'>
-  id: string
-  title: string
+	type: Exclude<LinkedinMedia['type'], 'article'>
+	id: string
+	title: string
 }
 
 export class LinkedinPost {
-  media?: LinkedinMediaArticle | Omit<LinkedinMediaAsset & { id: string }, 'source'>
+	media?: LinkedinMediaArticle | Omit<LinkedinMediaAsset & { id: string }, 'source'>
 
-  constructor(readonly text: string, readonly author: string) {}
+	constructor(readonly text: string, readonly author: string) {}
 
-  get payload() {
-    return {
-      author: this.author,
-      visibility: 'PUBLIC',
-      distribution: {
-        feedDistribution: 'MAIN_FEED',
-        targetEntities: [],
-        thirdPartyDistributionChannels: []
-      },
-      commentary: this.text,
-      lifecycleState: 'PUBLISHED',
-      isReshareDisabledByAuthor: false,
-      ...this.#parseMedia()
-    }
-  }
+	get payload() {
+		return {
+			author: this.author,
+			visibility: 'PUBLIC',
+			distribution: {
+				feedDistribution: 'MAIN_FEED',
+				targetEntities: [],
+				thirdPartyDistributionChannels: [],
+			},
+			commentary: this.text,
+			lifecycleState: 'PUBLISHED',
+			isReshareDisabledByAuthor: false,
+			...this.#parseMedia(),
+		}
+	}
 
-  addArticle(data: Omit<LinkedinMedia, 'type'>) {
-    this.media = {
-      type: LinkedinMediaTypes.ARTICLE,
-      ...data
-    }
-    return this
-  }
+	addArticle(data: Omit<LinkedinMedia, 'type'>) {
+		this.media = {
+			type: LinkedinMediaTypes.ARTICLE,
+			...data,
+		}
+		return this
+	}
 
-  addMedia(type: Exclude<LinkedinMedia['type'], 'article'>, title: string, id: string) {
-    this.media = {
-      type,
-      title,
-      id
-    }
-    return this
-  }
+	addMedia(type: Exclude<LinkedinMedia['type'], 'article'>, title: string, id: string) {
+		this.media = {
+			type,
+			title,
+			id,
+		}
+		return this
+	}
 
-  #parseMedia() {
-    if (!this.media) return {}
+	#parseMedia() {
+		if (!this.media) return {}
 
-    switch (this.media.type) {
-      case LinkedinMediaTypes.ARTICLE:
-        return {
-          content: {
-            article: {
-              source: this.media.source,
-              title: this.media.title,
-              ...(this.media.description && { description: this.media.description }),
-              ...(this.media.thumbnail && { thumbnail: this.media.thumbnail })
-            }
-          }
-        }
-      case LinkedinMediaTypes.IMAGE:
-      case LinkedinMediaTypes.DOCUMENT:
-      case LinkedinMediaTypes.VIDEO:
-        return {
-          content: {
-            media: {
-              id: this.media.id,
-              title: this.media.title
-            }
-          }
-        }
-    }
-  }
+		switch (this.media.type) {
+			case LinkedinMediaTypes.ARTICLE:
+				return {
+					content: {
+						article: {
+							source: this.media.source,
+							title: this.media.title,
+							...(this.media.description && { description: this.media.description }),
+							...(this.media.thumbnail && { thumbnail: this.media.thumbnail }),
+						},
+					},
+				}
+			case LinkedinMediaTypes.IMAGE:
+			case LinkedinMediaTypes.DOCUMENT:
+			case LinkedinMediaTypes.VIDEO:
+				return {
+					content: {
+						media: {
+							id: this.media.id,
+							title: this.media.title,
+						},
+					},
+				}
+		}
+	}
 }
--- src/networks/linkedin.ts
+++ src/networks/linkedin.ts
@@ -6,37 +6,37 @@ import { oak, z } from '../deps.ts'
 import validateBody from '../middlewares/zodValidationMiddleware.ts'
 
 const LinkedinMediaAssetInputSchema = z.object({
-  type: z.enum([LinkedinMediaTypes.IMAGE, LinkedinMediaTypes.DOCUMENT, LinkedinMediaTypes.VIDEO]),
-  source: z.string().url(),
-  title: z.string().max(100)
+	type: z.enum([LinkedinMediaTypes.IMAGE, LinkedinMediaTypes.DOCUMENT, LinkedinMediaTypes.VIDEO]),
+	source: z.string().url(),
+	title: z.string().max(100),
 })
 export type LinkedinMediaAssetInput = z.infer<typeof LinkedinMediaAssetInputSchema>
 
 const LinkedinMediaArticleInputSchema = z.object({
-  type: z.literal(LinkedinMediaTypes.ARTICLE),
-  source: z.string().url(),
-  thumbnail: z.string().url().optional(),
-  title: z.string().max(100).optional(),
-  description: z.string().max(300).optional()
+	type: z.literal(LinkedinMediaTypes.ARTICLE),
+	source: z.string().url(),
+	thumbnail: z.string().url().optional(),
+	title: z.string().max(100).optional(),
+	description: z.string().max(300).optional(),
 })
 export type LinkedinMediaArticleInput = z.infer<typeof LinkedinMediaArticleInputSchema>
 
 const LinkedinMediaSchema = z.discriminatedUnion('type', [
-  LinkedinMediaArticleInputSchema,
-  LinkedinMediaAssetInputSchema
+	LinkedinMediaArticleInputSchema,
+	LinkedinMediaAssetInputSchema,
 ])
 export type LinkedinMediaInput = z.infer<typeof LinkedinMediaSchema>
 
 const LinkedinShareInputSchema = z.object({
-  text: z.string().max(3000),
-  media: LinkedinMediaSchema.optional(),
-  comments: z
-    .array(
-      z.object({
-        text: z.string().max(3000)
-      })
-    )
-    .optional()
+	text: z.string().max(3000),
+	media: LinkedinMediaSchema.optional(),
+	comments: z
+		.array(
+			z.object({
+				text: z.string().max(3000),
+			}),
+		)
+		.optional(),
 })
 export type LinkedinShareInput = z.infer<typeof LinkedinShareInputSchema>
 
@@ -46,71 +46,71 @@ const client = new LinkedinClient(appConfig)
 const controller = new LinkedInController(appConfig, db, client)
 
 linkedin.get('/oauth/login', (ctx) => {
-  const loginUrl = controller.loginUrl
-  ctx.response.body = loginUrl
+	const loginUrl = controller.loginUrl
+	ctx.response.body = loginUrl
 
-  if (!ctx.request.url.searchParams.has('urlOnly')) {
-    ctx.response.headers.set('Location', loginUrl)
-    ctx.response.status = 302
-  }
-  return
+	if (!ctx.request.url.searchParams.has('urlOnly')) {
+		ctx.response.headers.set('Location', loginUrl)
+		ctx.response.status = 302
+	}
+	return
 })
 
 linkedin.get('/oauth/callback', async (ctx) => {
-  const params = ctx.request.url.searchParams
+	const params = ctx.request.url.searchParams
 
-  if (params.has('error')) {
-    throw oak.createHttpError(400, 'Error from Linkedin', {
-      cause: {
-        error: params.get('error'),
-        error_description: decodeURIComponent(params.get('error_description')!)
-      }
-    })
-  }
+	if (params.has('error')) {
+		throw oak.createHttpError(400, 'Error from Linkedin', {
+			cause: {
+				error: params.get('error'),
+				error_description: decodeURIComponent(params.get('error_description')!),
+			},
+		})
+	}
 
-  const code = params.get('code')
-  const state = params.get('state')
+	const code = params.get('code')
+	const state = params.get('state')
 
-  if (!code || !state) {
-    throw oak.createHttpError(422, 'Missing code or state')
-  }
+	if (!code || !state) {
+		throw oak.createHttpError(422, 'Missing code or state')
+	}
 
-  const csrfMatch = await controller.validateNonce(state)
-  if (!csrfMatch) {
-    throw oak.createHttpError(401, 'Invalid state')
-  }
+	const csrfMatch = await controller.validateNonce(state)
+	if (!csrfMatch) {
+		throw oak.createHttpError(401, 'Invalid state')
+	}
 
-  await controller.exchangeAccessToken(code, state)
+	await controller.exchangeAccessToken(code, state)
 
-  ctx.response.status = 200
-  ctx.response.body = {
-    status: 'ok',
-    message: 'Logged in successfully'
-  }
+	ctx.response.status = 200
+	ctx.response.body = {
+		status: 'ok',
+		message: 'Logged in successfully',
+	}
 })
 
 linkedin.get('/oauth/tokens', async (ctx) => {
-  await controller.validateAccessToken()
-  ctx.response.status = 200
-  ctx.response.body = {
-    status: 'ok',
-    message: 'Token is valid'
-  }
+	await controller.validateAccessToken()
+	ctx.response.status = 200
+	ctx.response.body = {
+		status: 'ok',
+		message: 'Token is valid',
+	}
 })
 
 linkedin.post(
-  '/',
-  validateBody(LinkedinShareInputSchema),
-  async (ctx: oak.Context<{ validatedBody: LinkedinShareInput }>) => {
-    await controller.validateAccessToken()
+	'/',
+	validateBody(LinkedinShareInputSchema),
+	async (ctx: oak.Context<{ validatedBody: LinkedinShareInput }>) => {
+		await controller.validateAccessToken()
 
-    const response = await controller.sharePost(ctx.state.validatedBody)
+		const response = await controller.sharePost(ctx.state.validatedBody)
 
-    ctx.response.status = 201
-    ctx.response.body = response
-    ctx.response.headers.set('Location', response.postUrl)
-    ctx.response.headers.set('X-LinkedIn-Post-URN', response.postUrn)
-  }
+		ctx.response.status = 201
+		ctx.response.body = response
+		ctx.response.headers.set('Location', response.postUrl)
+		ctx.response.headers.set('X-LinkedIn-Post-URN', response.postUrn)
+	},
 )
 
 export default linkedin
